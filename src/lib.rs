7e0b844a10d6fb180800d57c88cc1ab30197b2c7
Bruce Flynn <brucef@ssec.wisc.edu>
2020-02-07 09:53:23 -0600

    initial code for reading packet structure
    Reads both primary and secondary header (but not user data) providing
    the secondary header as an Option. I'm really not sure if this is a
    reasonable or idiomatic way to do this.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use packed_struct::prelude::*;
 use std::io;
 
 // XXX: Seems like it should be possible to get the size from the struct
-pub const PRIMARY_HEADER_SIZE: usize = 6;
+pub const PRIMARY_HDR_SIZE: usize = 6;
 pub const CDS_TIMECODE_SIZE: usize = 8;
 
 #[derive(PackedStruct, Debug)]
@@ -30,8 +30,8 @@ pub struct PrimaryHeader {
     len_minus1: u16,
 }
 
-pub fn decode_header(r: &mut impl io::Read) -> Result<PrimaryHeader, io::Error> {
-    let mut buf: [u8; PRIMARY_HEADER_SIZE] = [0; PRIMARY_HEADER_SIZE];
+pub fn read_header(r: &mut impl io::Read) -> Result<PrimaryHeader, io::Error> {
+    let mut buf: [u8; PRIMARY_HDR_SIZE] = [0; PRIMARY_HDR_SIZE];
     r.read_exact(&mut buf)?;
     match PrimaryHeader::unpack(&buf) {
         Ok(h) => Ok(h),
@@ -39,7 +39,7 @@ pub fn decode_header(r: &mut impl io::Read) -> Result<PrimaryHeader, io::Error> 
     }
 }
 
-#[derive(PackedStruct)]
+#[derive(PackedStruct, Debug)]
 #[packed_struct(endian = "msb", bit_numbering = "msb0")]
 pub struct CDSTimecode {
     #[packed_field(size_bits = "16")]
@@ -71,6 +71,49 @@ pub fn decode_cds(dat: u64) -> CDSTimecode {
     }
 }
 
+#[derive(PackedStruct, Debug)]
+#[packed_struct(endian = "msb", bit_numbering = "msb0")]
+pub struct JPSSSecondaryHeader {
+    #[packed_field(element_size_bytes = "8")]
+    timecode: CDSTimecode,
+    #[packed_field(size_bits = "8")]
+    num_packets: u8,
+    #[packed_field(size_bits = "8")]
+    _spare: u8,
+}
+
+const JPSS_SECONDARY_HDR_SIZE: usize = 10;
+
+pub fn read_jpss_secondary_header(r: &mut impl io::Read) -> Result<JPSSSecondaryHeader, io::Error> {
+    let mut buf: [u8; JPSS_SECONDARY_HDR_SIZE] = [0; JPSS_SECONDARY_HDR_SIZE];
+    r.read_exact(&mut buf)?;
+    match JPSSSecondaryHeader::unpack(&buf) {
+        Ok(h) => Ok(h),
+        Err(err) => Err(io::Error::new(io::ErrorKind::Other, err)),
+    }
+}
+
+pub struct Packet {
+    header: PrimaryHeader,
+    secondary_header: Option<JPSSSecondaryHeader>,
+}
+
+pub fn read_packet(r: &mut impl io::Read) -> io::Result<Packet> {
+    let ph = read_header(r)?;
+    if ph.has_secondary_header {
+        let sh = read_jpss_secondary_header(r)?;
+        return Ok(Packet{
+            header: ph,
+            secondary_header: Some(sh),
+        });
+    }
+
+    Ok(Packet{
+        header: ph,
+        secondary_header: None,
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -84,7 +127,7 @@ mod tests {
         ];
         let x = &dat[0..];
         let mut r = BufReader::new(x);
-        let ph = decode_header(&mut r).unwrap();
+        let ph = read_header(&mut r).unwrap();
 
         assert_eq!(ph.version.to_primitive(), 0);
         assert_eq!(ph.is_test, false);
@@ -107,5 +150,63 @@ mod tests {
 
         let ts = cds.timestamp();
         assert_eq!(ts.to_string(), "2016-01-01 00:00:00.167219 UTC");
+    }
+
+    #[test]
+    fn test_read_packet() {
+        let dat: [u8; 16] = [
+            // bytes from a SNPP CrIS packet
+            0xd, 0x59, 0xd2, 0xab, 0xa, 0x8f,
+            0x52, 0xc0, 0x0, 0x0, 0x0, 0xa7, 0x0, 0xdb, 1, 0,
+        ];
+        let x = &dat[0..];
+        let mut r = BufReader::new(x);
+        let packet = read_packet(&mut r).unwrap();
+
+        assert_eq!(packet.header.version.to_primitive(), 0);
+        assert_eq!(packet.header.is_test, false);
+        assert_eq!(packet.header.has_secondary_header, true);
+        assert_eq!(packet.header.apid.to_primitive(), 1369);
+        assert_eq!(packet.header.sequence_flags.to_primitive(), 3);
+        assert_eq!(packet.header.sequence_id.to_primitive(), 4779);
+        assert_eq!(packet.header.len_minus1, 2703);
+
+        match packet.secondary_header {
+            Some(sh) => {
+                assert_eq!(sh.timecode.days, 21184);
+        assert_eq!(sh.timecode.millis, 167);
+        assert_eq!(sh.timecode.micros, 219);
+                assert_eq!(sh.num_packets, 1);
+                assert_eq!(sh._spare, 0)
+            },
+            None => assert!(false),
+        };
+    }
+
+    #[test]
+    fn test_read_packet_no_secondary_header() {
+        let dat: [u8; 6] = [
+            // bytes from a SNPP CrIS packet with sec hdr flag set to 0
+            // no secondary header!
+            0x5, 0x59, 0xd2, 0xab, 0xa, 0x8f,
+        ];
+        let x = &dat[0..];
+        let mut r = BufReader::new(x);
+        let packet = read_packet(&mut r).unwrap();
+
+        assert_eq!(packet.header.version.to_primitive(), 0);
+        assert_eq!(packet.header.is_test, false);
+        assert_eq!(packet.header.has_secondary_header, false);
+        assert_eq!(packet.header.apid.to_primitive(), 1369);
+        assert_eq!(packet.header.sequence_flags.to_primitive(), 3);
+        assert_eq!(packet.header.sequence_id.to_primitive(), 4779);
+        assert_eq!(packet.header.len_minus1, 2703);
+
+        match packet.secondary_header {
+            Some(_) => {
+                assert!(false);
+            },
+            None => (),
+        };
     }
 }
