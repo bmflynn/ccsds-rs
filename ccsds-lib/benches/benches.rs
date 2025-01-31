97a641880ff02c408e3d511d0c7d483b594a02ef
Bruce Flynn <brucef@ssec.wisc.edu>
2025-01-31 13:40:17 -0600

    add bench for sync

--- ccsds-lib/benches/benches.rs
+++ ccsds-lib/benches/benches.rs
@@ -1,31 +1,61 @@
-use std::{fs::File, io::Read, path::PathBuf};
+use rand::{Rng, RngCore};
+use std::path::PathBuf;
 
-use ccsds::{DefaultPN, DefaultReedSolomon, PNDecoder, ReedSolomon};
+use ccsds::framing::{
+    DefaultDerandomizer, DefaultReedSolomon, Derandomizer, Integrity, IntegrityAlgorithm,
+    Synchronizer, ASM,
+};
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 
 fn fixture_path(name: &str) -> PathBuf {
-    let mut path = PathBuf::from(file!());
-    path.pop();
-    path.pop();
+    let mut path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     path.push(name);
     path
 }
 
-use rand::Rng;
+fn bench_synchronization(c: &mut Criterion) {
+    let data: [u8; 1024] = {
+        let mut x = [0u8; 1024];
+        let mut rng = rand::thread_rng();
+        rng.fill_bytes(&mut x);
+        x
+    };
+
+    let mut group = c.benchmark_group("sync");
+    group.throughput(Throughput::Bytes(1024));
+    group.bench_function("random_data", |b| {
+        b.iter(|| {
+            let sync = Synchronizer::new(&data[..], &ASM, 1024);
+            let _: Vec<Vec<u8>> = sync.into_iter().filter_map(Result::ok).collect();
+        });
+    });
+
+    group.finish();
+}
 
 fn bench_rs_correct_codeblock(c: &mut Criterion) {
-    // Read a single CADU from the fixture file
-    let mut file = File::open(fixture_path("tests/fixtures/snpp_synchronized_cadus.dat")).unwrap();
-    let mut block = [0u8; 1024]; // ASM + codeblock
-    file.read_exact(&mut block).unwrap();
-    let block = &block[4..];
+    let mut block = std::fs::read(fixture_path("benches/snpp_block.dat")).unwrap();
+
+    // introduced some errors
+    let block = {
+        let mut rng = rand::thread_rng();
+        let b = &mut block[4..];
+        let idx: usize = rng.gen::<u8>().into();
+        b[idx] = b[idx].wrapping_add(1);
+        b
+    };
 
     let mut group = c.benchmark_group("rs");
     group.throughput(Throughput::Bytes(1020));
     group.bench_function("correct_codeblock", |b| {
         b.iter(|| {
-            let rs = DefaultReedSolomon;
-            let _ = rs.correct_codeblock(block, 4).unwrap();
+            let rs = DefaultReedSolomon::new(4);
+            let (i, _) = rs.perform(block).unwrap();
+            assert_eq!(
+                i,
+                Integrity::Corrected,
+                "expected to have corrected block; got {i:?}"
+            );
         });
     });
     group.finish();
@@ -35,21 +65,26 @@ fn bench_rs_correct_codeblock(c: &mut Criterion) {
 fn bench_pndecode(c: &mut Criterion) {
     let mut rng = rand::thread_rng();
     let mut buf = [0u8; 1020];
-    for i in 0..buf.len() {
+    for b in buf.iter_mut() {
         let f: u8 = rng.gen();
-        buf[i] = f;
+        *b = f;
     }
 
     let mut group = c.benchmark_group("pn");
     group.throughput(Throughput::Bytes(buf.len() as u64));
     group.bench_function("decode", |b| {
         b.iter(|| {
-            let pn = DefaultPN;
-            let _ = pn.decode(&buf.clone());
+            let pn = DefaultDerandomizer;
+            let _ = pn.derandomize(&buf.clone());
         });
     });
     group.finish();
 }
 
-criterion_group!(benches, bench_pndecode, bench_rs_correct_codeblock);
+criterion_group!(
+    benches,
+    bench_pndecode,
+    bench_rs_correct_codeblock,
+    bench_synchronization
+);
 criterion_main!(benches);
